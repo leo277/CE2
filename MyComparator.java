e429d993f96cbfbc04c8a893eb8ac923b42c191c
zhang yongkai <yongkai8@hotmail.com>
2014-09-17 23:02:09 +0800

    added introduction for the MyComparator class

--- MyComparator.java
+++ MyComparator.java
@@ -1,8 +1,18 @@
+/**
+ * This is user-defined comparator. This class will allow 
+ * the two strings to be compared with case-insensitivity. 
+ *
+ * @project: TextBuddy ++ 
+ * @author : Zhang yongkai
+ * @matric : A0110567L
+ * @tutorialID : T11
+ * 
+ */
+
 import java.util.Comparator;
 
-
-public class MyComparator implements Comparator<String>{
-	public int compare(String textA, String textB){
+public class MyComparator implements Comparator<String> {
+	public int compare(String textA, String textB) {
 		return textA.compareToIgnoreCase(textB);
 	}
 
